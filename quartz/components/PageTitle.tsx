62b35554c90da5aecfe9ac7433f69215797d9a87
joelcross <joeldcross@gmail.com>
2025-03-27 14:10:40 +0100

    More mobile styling

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -13,7 +13,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
       <h2 class={classNames(displayClass, "page-title")}>
         <a href={baseDir}>{title}</a>
       </h2>
-      <img src={imgSrc} style="margin: 0;" />
+      <img class={classNames(displayClass, "page-title")} src={imgSrc} style="margin: 0;" />
     </>
   )
 }
